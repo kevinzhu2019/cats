7c6d9cd8550cf557bbf055c70ec0ac62780bcb71
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-12-06 05:27:01 +0000

    adding mocha testing with callback

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => { //must passing the err first
+    // CHANGE 1: Pass data back via callback instead of return.
+    console.log('Callback: I have the data!')
+    if (error) {
+      callback(undefined);
+    }else {callback(data);} 
+  });
+}
+
+// CHANGE 2: Value now comes back via callback, not return value
+breedDetailsFromFile('Bombay', (bombay) => {
+  console.log('Return Value: ', bombay) // => print out details correctly.
+});
+
+module.exports = breedDetailsFromFile;
